9377cff1b97a79bcdb9dd36877e3f294d209c997
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2022-05-23 16:39:47 +0400

    Settings Page Start 1

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -6,7 +6,49 @@ export class Settings extends React.Component{
 	  return (
 		<React.Fragment>
 			<div id="setingsList">
-			  
+			  <ul>
+				  <li className="setting-button wcc-id">
+					<img src="" className="wcc-id-avatar" />
+					<div>
+						<strong>WCC ID - очень скоро!</strong>
+						<span>Все возможности Навигатора и не только в одной учётной  записи</span>
+					</div>
+				  </li>
+				  <li className="setting-button parameter-menu">
+						<li>
+							<img src="" />
+							<span>Навигатор</span>
+						</li>
+						<li>
+							<img src="" />
+							<span>Поиск и списки</span>
+						</li>
+						<li>
+							<img src="" />
+							<span>Уведомления</span>
+						</li>
+						<li>
+							<img src="" />
+							<span>Безопасность</span>
+						</li>
+				  </li>
+				  <li className="setting-button parameter-menu">
+					<ul>
+						<li>
+							<img src="" />
+							<span>О навигаторе</span>
+						</li>
+						<li>
+							<img src="" />
+							<span>Политика конфедициальности данных WCC</span>
+						</li>
+						<li>
+							<img src="" />
+							<span>Приложения и сервисы WCC</span>
+						</li>
+					</ul>
+				  </li>
+			  </ul>
 			</div>
 		</React.Fragment>
 	  );
